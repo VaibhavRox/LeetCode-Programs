6bacb247181efabd33c3702d790a62b06efb545f
VaibhavRox <vaibhavrox710@gmail.com>
2025-08-06 22:35:47 +0530

    21st commit

---
+++ DeleteCharstomakeFancyString.py
@@ -0,0 +1,14 @@
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        res=s[0]
+        last=s[0]
+        count=1
+        for i in range(1,len(s)):
+            if s[i]!=last:
+                last=s[i]
+                count=0
+            count+=1
+            if count>2:
+                continue
+            res+=s[i]
+        return res
